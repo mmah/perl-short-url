51f6f765cfd671034537c213639e287faf1362a0
William Page <wpage@vetstreet.com>
2016-06-29 09:28:24 -0400

    First cut at a VetStreet self-hosted short URL system

--- lib/ShortURL.pm
+++ lib/ShortURL.pm
@@ -2,17 +2,51 @@ package ShortURL;
 
 use strict;
 use warnings;
-use WWW::Curl::Easy;
+use URI::Escape;
+use Math::BaseCalc;
+use DBI;
+use Time::HiRes;
+
+my $hash_code  = 9036507220;
+my $ShortURL_Table = 'sandbox.short_urls';
+
 
 sub new {
-   my ($class) = @_;
-
+   my ($class, $Parameters) = @_;
+   
+   my $DB_Name     = 'vidb05'; 
+   my $DB_User     = 'perl_app';
+   my $DB_Password = 'Titus3:2';
+   
    my $self = {
-      curl    => WWW::Curl::Easy->new,
-      retcode => 0
+      basecalc => new Math::BaseCalc(digits=>[0..9, 'a'..'z', 'A'..'Z']),
+      retcode  => 0
    };
    bless $self, $class;
 
+   if (exists($Parameters->{db_conn})) {
+      $self->{db_conn} = $Parameters->{db_conn};
+   } elsif (exists($Parameters->{db_host})) {
+      local $@; # Don't step on other code.
+      my $result = undef;
+      eval {
+         local $SIG{__DIE__}; # No sigdie handler
+         $self->{db_conn} =  DBI->connect("dbi:Pg:dbname=$DB_Name;" .
+                                          "host=$Parameters->{db_host}",
+                                          $DB_User,
+                                          $DB_Password)
+                     or die "Could not connect: $DBI::errstr";
+      };
+   } else {
+      die "Database information not supplied";
+   }
+   
+   if (exists($Parameters->{time_to_live})) {
+      $self->{expires} = "$Parameters->{time_to_live} days";
+   } else {
+      $self->{expires} = '90 days';
+   }
+   
    return $self;
 }
 #=======================================================================
@@ -21,49 +55,45 @@ sub new {
 sub Get {
    my ($self, $LongURL) = @_;
    
-   $self->{curl}->setopt(CURLOPT_HEADER,1);
-   $self->{curl}->setopt(CURLOPT_HTTPHEADER,['Content-Type: application/json']);
-   $self->{curl}->setopt(CURLOPT_POST, 1);
-   $self->{curl}->setopt(CURLOPT_POSTFIELDS,'{"longUrl": "' . $LongURL . '"}');
-   $self->{curl}->setopt(CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyDzXn5c4Pp5El0oRVy32hwD_KMwO3RU5gs');
+   $LongURL = uri_escape($LongURL);
+   
+   my $short_url = undef;
+   
+   while (1) {
+      # Make a short url code...
+      $short_url = $self->basecalc->to_base(int(Time::HiRes::time()*1000) ^ $hash_code);
+      
+      # Make sure we haven't used this short code before...
+      my $sql = <<"END_SQL";
+      select count(*) as count from $ShortURL_Table where short_url = $short_url;
+END_SQL
 
-   my $response_body;
-   $self->{curl}->setopt(CURLOPT_WRITEDATA,\$response_body);
-
-   # Starts the actual request
-   my $retcode = $self->{curl}->perform;
-   $self->{retcode} = $retcode;
-
-   # Looking at the results...
-   if ($retcode == 0) {
-      #print("Transfer went ok\n");
-      my $response_code = $self->{curl}->getinfo(CURLINFO_HTTP_CODE);
-      # judge result and next action based on $response_code
-      #print("Received response: $response_body\n");
-      # We expect to find the short URL in the json result in the form "id": "http://goo.gl/fbsS"
-      $response_body =~ m/"id":\s*"([^"]*)"/;
-      return $1;
-   } else {
-      # Error code, type of error, error message
-      print("An error happened: $retcode " . $self->{curl}->strerror($retcode) . " " . $self->{curl}->errbuf."\n");
-      return undef;
+      my $sth = $self->{db_conn}->prepare($sql) ||
+          die "Unable to prepare $sql, $self->{db_conn}->errstr";
+          
+      if (my $datarow = $sth->fetchrow_hashref()) {
+         # We got a row...check the count...
+         if ($datarow > 0) {
+            next;
+         }
+      }
+      
+      # If we didn't find the code in the table, we can use this code, so exit...
+      last;
    }
-}
-#=======================================================================
-
-
-sub ReturnCode {
-   my ($self) = @_;
    
-   return $self->{curl}->strerror($self->{retcode});
-}
-#=======================================================================
+   # Install the new code in the table...
+   my $sql = <<"END_SQL";
+insert into $ShortURL_Table
+   (short_url, long_url, expire_date)
+   values
+   ($short_url, $LongURL, now()::date+'$self->{expires}'::interval);
+END_SQL
 
-
-sub ErrorText {
-   my ($self) = @_;
+   my $result = $self->{db_conn}->do($sql) or die "Unable to do $sql, $self->{db_conn}->errstr";
    
-   return $self->{curl}->errbuf;
+   # Return the short code to the caller...
+   return $short_url;
 }
 #=======================================================================
 
