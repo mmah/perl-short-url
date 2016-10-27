9ae02efed73ac3e8bda5f82cad353108b3c7edb2
William S. Page <pagews@pageassoc.com>
2016-10-27 15:44:14 -0400

    Updated the table create sql to final configuration

--- sql/create_provider_table.sql
+++ sql/create_provider_table.sql
@@ -1,8 +1,34 @@
-drop table if exists sand_box.short_url_log;
-create table sand_box.short_url_log (
-   short_url            text primary key,
-   long_url             text not null,
-   create_time          timestamp default now(),
-   created_by           text not null,
-   expires              timestamp default (now()+'1 year'::interval)
+-- Table: short_url_lookup
+
+-- DROP TABLE short_url_lookup;
+
+CREATE TABLE short_url_lookup
+(
+  id serial NOT NULL,
+  short_url text NOT NULL,
+  long_url text NOT NULL,
+  app_name text NOT NULL,
+  expire_date timestamp without time zone NOT NULL DEFAULT (now() + '1 year'::interval),
+  create_user text NOT NULL DEFAULT "current_user"(),
+  create_date timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT short_url_lookup_pkey PRIMARY KEY (id),
+  CONSTRAINT short_url_lookup_uniq1 UNIQUE (short_url),
+  CONSTRAINT short_url_lookup_app_name_check CHECK (app_name = ANY (ARRAY['HCRE'::text, 'HCRP'::text]))
+)
+WITH (
+  OIDS=FALSE
 );
+ALTER TABLE short_url_lookup
+  OWNER TO vidb;
+GRANT ALL ON TABLE short_url_lookup TO vidb;
+GRANT ALL ON TABLE short_url_lookup TO perl_app;
+
+-- Index: short_url_lookup_idx1
+
+-- DROP INDEX short_url_lookup_idx1;
+
+CREATE UNIQUE INDEX short_url_lookup_idx1
+  ON short_url_lookup
+  USING btree
+  (short_url);
+
