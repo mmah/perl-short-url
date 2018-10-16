66e7ec676ce26cbb6439743fc188dfc83149d331
Eric Heath <eric.m.heath@gmail.com>
2018-10-16 16:29:30 -0400

    Specified default creator

--- lib/ShortURL.pm
+++ lib/ShortURL.pm
@@ -19,42 +19,44 @@ my $ShortURL;
 
 sub new {
    my ($class, $params) = @_;
-   
+
    # Return the logger object if already created...
    if (defined($ShortURL)) {
       return $ShortURL;
    }
-   
-   # Get the logger object which should have been created by the host 
+
+   # Get the logger object which should have been created by the host
    #    applicaion.
    $Logger = Logger->GetLogger();
 
-   # Get a database object.  We would normally expect the host program to 
+   # Get a database object.  We would normally expect the host program to
    #    have created a database object but if not, we will pass along the
    #    the parameters given us in case the host application wants us to
    #    make the DB connection.
    $DB_Connection = DB_Connect->GetConnection($params);
-   
+
    my $self = {
-                 
+
               };
-              
+
    bless $self, $class;
-   
+
    # Capture the creator identity if specified...
    if (exists($params->{creator})) {
       $self->{creator} = $params->{creator};
    } else {
-      die "Fatal: The short URL requestor was not supplied";
+      $self->{creator} = 'HCRE';
+      # die "Fatal: The short URL requestor was not supplied";
+    }
    }
-   
+
    # Capture the creator identity if specified...
    if (exists($params->{ttl})) {
       $self->{ttl} = $params->{ttl};
    } else {
       $self->{ttl} = '1 year';
    }
-   
+
    return $self;
 }
 #=======================================================================
@@ -62,7 +64,7 @@ sub new {
 
 sub Get {
    my ($self, $LongURL) = @_;
-   
+
    if (lc(substr($LongURL, 0, 4)) != "http") {
       $LongURL = "http://" . $LongURL;
    }
@@ -85,7 +87,7 @@ sub Get {
       # Now we have a unique code...insert it...
       $DB_Connection->DoIO("insert into $TableName (long_url, short_url, app_name, expire_date) values ('$LongURL', '$short_code', '$self->{creator}', now()+'$self->{ttl}'::interval);");
    }
-   
+
    return "http://vst.vet/" . $short_code;
 }
 #=======================================================================
@@ -95,7 +97,7 @@ sub encode_base36 {
     my ($number) = @_;
 
     die 'Invalid base10 number'  if $number    =~ m{\D};
-    
+
     #print "number in = '$number'\n";
     my $result = '';
     while ( $number ) {
